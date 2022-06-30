d64083590c205828e411b5e7d5ee4e605c5c71ed
Uyai-Abasi <isidoreuyaiabasi@gmail.com>
2022-06-30 09:54:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function inputColor(color){
+    
+    if (color == "red" || color == "RED" || color =="Red"){
+       console.log("STOP")
+    }
+    else if (color == "yellow" || color == "YELLOW" || color == "Yellow"){
+        console.log("GET READY")
+    }
+    else if (color == "green" || color == "GREEN" || color == "Green"){
+        console.log("GO")
+    }
+    else{
+        console.log("OOPS!")
+    }
+    
+    }
+    inputColor("GREEN")
